4ed18b884c17088ee98bcdbaea3d07a4a42af956
Samarjeet shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2021-04-14 18:31:29 +0530

    Create Selection sort.c

---
+++ Selection sort.c
@@ -0,0 +1,7 @@
+/*Selection sort in C to sort numbers of an array in ascending order. With a little modification, it arranges numbers in descending order.
+
+Selection sort algorithm (for ascending order)
+Find the minimum element in the array and swap it with the element in the 1st position.
+Find the minimum element again in the remaining array[2, n] and swap it with the element at 2nd position, now we have two elements at their correct positions.
+We have to do this n-1 times to sort the array.
+*/
