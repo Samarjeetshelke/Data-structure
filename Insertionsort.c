40e7ca72bdf204620993082d0caaee6952e76e14
Samarjeet shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2021-05-04 07:29:45 +0530

    Update Insertionsort.c

--- Insertionsort.c
+++ Insertionsort.c
@@ -2,7 +2,12 @@ link :-"https://www.geeksforgeeks.org/insertion-sort/";
 
 /*  insertion sort:-
 
-
+Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
+Algorithm 
+To sort an array of size n in ascending order: 
+1: Iterate from arr[1] to arr[n] over the array. 
+2: Compare the current element (key) to its predecessor. 
+3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements one position up to make space for the swapped element
 
 */
 code:-
