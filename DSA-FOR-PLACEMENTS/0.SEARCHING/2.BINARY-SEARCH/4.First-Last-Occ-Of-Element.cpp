68c13aca8510a075d5ccc54880a2ee75e442551d
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2024-07-10 12:38:32 +0530

    Create 4.First-Last-Occ-Of-Element.cpp

---
+++ DSA-FOR-PLACEMENTS/0.SEARCHING/2.BINARY-SEARCH/4.First-Last-Occ-Of-Element.cpp
@@ -0,0 +1,53 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int firstOcc(int arr[],int n,int x){
+    int s =0,e=n-1;
+    int ans=-1;
+    
+    while(s<=e){
+        int mid = (s+e)/2;
+        
+        if(arr[mid]==x){
+            ans = mid;
+            e=mid-1;
+        }
+        else if(x>arr[mid]){
+            s=mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+    }
+    return ans;
+}
+
+int lastOcc(int arr[],int n,int x){
+    int s =0,e=n-1;
+    int ans=-1;
+    
+    while(s<=e){
+        int mid = (s+e)/2;
+        
+        if(arr[mid]==x){
+            ans = mid;
+            s=mid+1;
+        }
+        else if(x>arr[mid]){
+            s=mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+    }
+    return ans;
+}
+int main() {
+    int arr[] = {1,2,3,4,5,5,5,5,6,7,8,9,12};
+    
+    cout<<"The First Occurnce of Element : "<<firstOcc(arr,(sizeof(arr)/sizeof(int)),5);
+    cout<<"The Last Occurnce of Element : "<<lastOcc(arr,(sizeof(arr)/sizeof(int)),5);
+
+    return 0;
+}
