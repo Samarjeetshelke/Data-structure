364c48177603d5d267398e5fef91651228e94a94
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2024-07-10 16:02:46 +0530

    Update 5.Peak-Index-In-Mountain.cpp

--- DSA-FOR-PLACEMENTS/0.SEARCHING/2.BINARY-SEARCH/5.Peak-Index-In-Mountain.cpp
+++ DSA-FOR-PLACEMENTS/0.SEARCHING/2.BINARY-SEARCH/5.Peak-Index-In-Mountain.cpp
@@ -30,3 +30,27 @@ int main() {
 
     return 0;
 }
+
+
+//more optimised
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 0;
+        int end = arr.size()-1;
+        int mid = start + (end-start)/2;
+
+        while( start<=end ){
+            if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            else if(arr[mid] < arr[mid+1]){
+                start = mid+1;
+            }
+            mid = start + (end-start)/2; 
+        }
+        return -1;
+    }
