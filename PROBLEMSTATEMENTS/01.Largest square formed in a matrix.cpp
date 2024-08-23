7b1b1d8e87c203348b75e5ab9a2100321d13a76c
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2024-08-23 07:39:33 +0530

    Update 01.Largest square formed in a matrix.cpp

--- PROBLEMSTATEMENTS/01.Largest square formed in a matrix.cpp
+++ PROBLEMSTATEMENTS/01.Largest square formed in a matrix.cpp
@@ -26,3 +26,36 @@ class Solution {
     }
 
 };
+//using DP
+
+int solve(vector<vector<int>>& mat,int i,int j,int &maxi,vector<vector<int>>& dp){
+    if(i>=mat.size()||j>=mat[0].size()){
+        return 0;
+    }
+    
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    
+    
+    int r = solve(mat,i,j+1,maxi,dp);
+    int d = solve(mat,i+1,j,maxi,dp);
+    int dig = solve(mat,i+1,j+1,maxi,dp);
+    
+    if(mat[i][j] == 1){
+        dp[i][j] = 1+min({r,d,dig});
+        maxi=max(maxi,dp[i][j]);
+        return dp[i][j];
+    }
+    else{
+        dp[i][j]=0;
+        return dp[i][j];
+    }
+    
+}
+int maxSquare(int n, int m, vector<vector<int>> mat) {
+    int maxi = 0;
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    solve(mat, 0, 0, maxi, dp);
+    return maxi;
+}
